6aa48e7035625882c66bf9b5cc9c4a6f2c4fbad9
Brenda <bee@bee.com>
2022-06-07 00:04:44 +0300

    Adds the Post models

--- insta_glam/models.py
+++ insta_glam/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='posts')
+    caption = models.CharField(max_length=255)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.author.username} post'
+
+    
