4098106fe8175843a70c32f04d85e68520838b60
Brenda <bee@bee.com>
2022-06-07 00:06:02 +0300

    Adds the Comment model

--- insta_glam/models.py
+++ insta_glam/models.py
@@ -20,3 +20,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.Case)
+    body = models.TextField(max_length=500, blank=False)
+
+    def __str__(self):
+        return self.body
+
+        
