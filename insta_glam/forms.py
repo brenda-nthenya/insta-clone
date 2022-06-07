972a6233822be1c5541107362099bb40c5ec645a
Brenda <bee@bee.com>
2022-06-07 14:09:30 +0300

    Creates the comment, profile and registration form

--- insta_glam/forms.py
+++ insta_glam/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Comment
+from .models import Comment, Profile
 
 class UserRegisterForm(UserCreationForm):
 	email = forms.EmailField()
@@ -14,3 +14,8 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['author','post','comment']
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image','bio']
