a0d2413c643128fb7ccb2a6e89c99c329c9b277c
Brenda <bee@bee.com>
2022-06-07 13:28:18 +0300

    Adds the like, caption and post date to the Posts model

--- insta_glam/models.py
+++ insta_glam/models.py
@@ -1,36 +1,58 @@
 from django.db import models
-from cloudinary.models import CloudinaryField
+from django.urls import reverse
+# from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# from autoslug import AutoSlugField
+from PIL import Image
+
 # Create your models here.
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='posts')
     caption = models.CharField(max_length=255)
+    post_date = models.DateTimeField(default=timezone.now)
     likes = models.IntegerField(default=0)
-    date_posted = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f'{self.author.username} post'
-
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
     
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='', upload_to='profile_pics')
-    profile_caption = models.CharField(max_length=255)
+    bio = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return str(self.user.username)
+
+    def get_absolute_url(self):
+        return "/users/{}".format(self.slug)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.Case)
-    body = models.TextField(max_length=500, blank=False)
+    comment = models.TextField(max_length=500, blank=False)
+    comment_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.body
+        return self.comment
 
-class Like(models.Model):
-    user = models.ForeignKey(User, related_name='likes', on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+    
