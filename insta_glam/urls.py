dbdb1a783261e8be2da0bd480d59f45c151cd47e
Brenda <bee@bee.com>
2022-06-07 13:23:00 +0300

    Adds the urls to the various sides of the page

---
+++ insta_glam/urls.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from . import views
+from django.contrib.auth import views as  auth_views
+from django.conf.urls.static import static
+from django.urls import path
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register', views.register, name="register-authentication"),
+    path('accounts/profile/',views.profile_info,name='profile'),
+    path('profile_edit/', views.profile_edit, name='profile_edit'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('search', views.search_results, name='search_results'),
+    path('edit/', views.profile_edit, name='edit'),
+    path('add_comment/', views.CommentCreateView.as_view(),name='add_comment'),
+    
+]
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+    
