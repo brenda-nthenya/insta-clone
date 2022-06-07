83040860d7e259b1943385cfa713dbdcde06846b
Brenda <bee@bee.com>
2022-06-07 13:27:16 +0300

    Adds the view functions of the different pages

--- insta_glam/views.py
+++ insta_glam/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+# from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import *
+from .forms import *
+from django.contrib import messages
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -16,7 +20,104 @@ def index(request):
         "comments":comments,
         "posts": posts,
         "profiles": profiles,
-        "user_profile":user_profile
+        # "user_profile":user_profile
     }
 
     return render(request, 'index.html', context)
+
+@login_required
+def add_comment(request):
+    current_user = request.user
+
+    if request.method == 'POST':
+        c_form = CommentForm(request.POST, instance=request.user)
+    else:
+        c_form = CommentForm(instance=request.user)
+
+    if c_form.is_valid():
+        print(c_form)
+        c_form.save()
+
+        messages.success(
+            request, f'comment created successfully')
+        return redirect('index')
+    context = {
+        'c_form': c_form
+    }
+    return render(request, 'comment_form.html', context)
+
+@login_required
+def profile_info(request):
+    profiles = Profile.objects.filter(user=request.user)
+    if not profiles.first():
+        profile = Profile.objects.create(user=request.user)
+        profile.save()
+        profile = Profile.objects.get(user=request.user)
+        posts = Post.objects.filter(author=request.user)
+        context = {
+           "profiles": profile,
+           "posts":posts
+    }
+
+    return render(request, 'profile/profile.html')
+    # return HttpResponse()    
+
+
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    fields = ['author', 'post', 'comment']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+def profile_edit(request):
+    current_user = request.user
+
+    if request.method == 'POST':
+        p_form = ProfileEditForm(
+            request.POST, request.FILES,instance=request.user.profile
+        )
+    else:
+        p_form = ProfileEditForm(instance=request.user.profile)
+
+    if  p_form.is_valid():
+        p_form.save()
+
+        messages.success(
+            request, f'Your profile has been updated successfully')
+        return redirect('profile_edit')
+    context = {
+        'p_form': p_form
+    }
+    return render(request, 'profile/edit_profile.html', context)
+
+def search_results(request):
+
+    if 'article' in request.GET and request.GET["article"]:
+        name = request.GET.get("article")
+        users= Profile.get_user(name)
+        message = f"{name}"
+
+        return render(request, 'search_results.html',{"message":message,"users": users})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search_results.html', {"message": message})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data('username')
+            messages.success(f'account for {username} created successfully')
+            print(form.cleaned_data)
+            profile = Profile.objects.create(user=form.cleaned_data)
+            profile.save()
+            return redirect('login')
+        else:
+            form = UserCreationForm()
+        return render(request, 'django_registration/registration_form.html', {form:form})
