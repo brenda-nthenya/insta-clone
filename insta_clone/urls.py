71449d33b1162f54a011b6f9e13c554499c9a722
Brenda <bee@bee.com>
2022-06-07 15:44:41 +0300

    Adds the links to django-registration and logout urls

--- insta_clone/urls.py
+++ insta_clone/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('insta_glam.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('logout/', views.LogoutView.as_view(
+        template_name="registration/logout.html"
+    ))
 ]
