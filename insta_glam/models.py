d09c3bbfce03b7e3d3631415af93dec0c86e6924
Brenda <bee@bee.com>
2022-06-10 10:04:09 +0300

    Changes the structure of the models.Adds the follow model

--- insta_glam/models.py
+++ insta_glam/models.py
@@ -11,48 +11,82 @@ from PIL import Image
 
 # Create your models here.
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    image = models.ImageField(default='', upload_to='profile_pics')
+    bio = models.CharField(max_length=255)
+    # slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    name = models.CharField(blank=True, max_length=120)
+
+    
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def save_profile(self):
+        self.user
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def search_profile(cls, name):
+        return cls.objects.filter(user__username__icontains=name).all()
+
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='posts')
     caption = models.CharField(max_length=255)
     post_date = models.DateTimeField(default=timezone.now)
-    likes = models.IntegerField(default=0)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts', null=True)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True, )
 
+    
     def __str__(self):
-        return f'{self.author.username} post'
+        return f'{self.author.username} Post'
     
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        
-        img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
-    
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='', upload_to='profile_pics')
-    bio = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
-    
-
-    def __str__(self):
-        return str(self.user.username)
+    class Meta:
+        ordering = ["-pk"]
 
     def get_absolute_url(self):
-        return "/users/{}".format(self.slug)
+        return f"/post/{self.id}"
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+    @property
+    def get_all_comments(self):
+        return self.comments.all()
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def total_likes(self):
+        return self.likes.count()
 
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.Case)
+    post = models.ForeignKey(Post, on_delete=models.Case, related_name='comments')
     comment = models.TextField(max_length=500, blank=False)
     comment_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.comment
+
+class Follow(models.Model):
+    follower = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='followers')
+
+    def __str__(self):
+        return f'{self.follower} Follow'
 
     
